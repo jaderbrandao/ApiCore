ee0999a2a67e5f2197db41a80645b682755283b8
Jader Brandão <33245908+jaderbrandao@users.noreply.github.com>
2021-01-19 11:59:48 -0300

    Foi criado as extensões para usar o swagger e versionamento de api's.

---
+++ APICore/Extensions/APIVersioningExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace APICore.Extensions
+{
+    public static class APIVersioningExtension
+    {
+        public static void UseAPiVersioning(this IServiceCollection services, string applicationName)
+        {
+            services.AddApiVersioning(
+            options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                options.ReportApiVersions = true;
+            });
+            services.AddVersionedApiExplorer(
+            options =>
+            {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                options.GroupNameFormat = "'v'VVV";
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddSwaggerGen(options =>
+            {
+                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+                foreach (var description in provider.ApiVersionDescriptions)
+                    options.SwaggerDoc(description.GroupName, new OpenApiInfo() { Title = $"{applicationName} {description.ApiVersion}", Version = description.ApiVersion.ToString() });
+
+                options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                var fileXml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var pathXml = Path.Combine(AppContext.BaseDirectory, fileXml);
+                options.IncludeXmlComments(pathXml);
+                options.CustomSchemaIds(x => x.FullName);
+                options.DescribeAllParametersInCamelCase();
+            });
+        }
+    }
+}
---
+++ APICore/Extensions/SwaggerExtension.cs
@@ -0,0 +1,31 @@
+﻿using APICore.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APICore.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.OperationFilter<SwaggerDefaultValues>();
+            });
+        }
+
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                // build a swagger endpoint for each discovered API version
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+            });
+        }
+    }
+}
