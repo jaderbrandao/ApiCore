2a7c5a1b3c4bab462ebeb0ea2e1a0c1cf1d7ae49
Jader Brandão <33245908+jaderbrandao@users.noreply.github.com>
2021-01-19 14:12:06 -0300

    Foi implementado a injeção de dependencia da IApiVersionDescriptionProvider.

--- APICore/Extensions/SwaggerExtension.cs
+++ APICore/Extensions/SwaggerExtension.cs
@@ -1,19 +1,55 @@
 ﻿using APICore.Swagger;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace APICore.Extensions
 {
     public static class SwaggerExtension
     {
-        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        public static void AddSwaggerConfiguration(this IServiceCollection services, string applicationName)
         {
             services.AddSwaggerGen(s =>
             {
                 s.OperationFilter<SwaggerDefaultValues>();
+            });
+
+           services.AddApiVersioning(
+           options =>
+           {
+               options.AssumeDefaultVersionWhenUnspecified = true;
+               options.DefaultApiVersion = new ApiVersion(1, 0);
+                // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions"
+                options.ReportApiVersions = true;
+           });
+            services.AddVersionedApiExplorer(
+            options =>
+            {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                options.GroupNameFormat = "'v'VVV";
+                // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen(options =>
+            {
+                options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                var fileXml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var pathXml = Path.Combine(AppContext.BaseDirectory, fileXml);
+                options.IncludeXmlComments(pathXml);
+                options.CustomSchemaIds(x => x.Name);
+                options.DescribeAllParametersInCamelCase();
             });
         }
 
